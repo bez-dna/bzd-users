2aa163d34301210152cdab7256a167d04c4acdeb
Andrei Brizhinev <bragovo@gmail.com>
2025-08-31 18:17:45 +0300

    Remove lib

--- lib/migration/src/main.rs
+++ lib/migration/src/main.rs
@@ -1,6 +1,7 @@
+use bzd_users_migration::Migrator;
 use sea_orm_migration::prelude::*;
 
 #[async_std::main]
 async fn main() {
-    cli::run_cli(migration::Migrator).await;
+    cli::run_cli(Migrator).await;
 }
